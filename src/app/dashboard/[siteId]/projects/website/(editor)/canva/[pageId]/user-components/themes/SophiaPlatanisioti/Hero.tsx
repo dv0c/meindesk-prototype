6a9784d18315a9424867906f2f9815bcdd25069b
meindesk <tasosmeidanis12@gmail.com>
2026-01-20 12:26:10 +0200

    modified:   src/app/dashboard/[siteId]/projects/website/(editor)/canva/[pageId]/user-components/themes/SophiaPlatanisioti/Hero.tsx

--- src/app/dashboard/[siteId]/projects/website/(editor)/canva/[pageId]/user-components/themes/SophiaPlatanisioti/Hero.tsx
+++ src/app/dashboard/[siteId]/projects/website/(editor)/canva/[pageId]/user-components/themes/SophiaPlatanisioti/Hero.tsx
@@ -32,7 +32,7 @@ const defaultProps: HeroProps = {
     thumbnail: 'https://images.unsplash.com/photo-1764377849133-635add556929?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     heading1: 'The First Chapter',
     heading2: 'A New Beginning',
-    fontFamily: 'font-sans',
+    fontFamily: 'font-serif',
 }
 
 // -----------------------------------------------------------------------------
